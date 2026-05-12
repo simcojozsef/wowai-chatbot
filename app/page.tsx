ace251a711fb6e203acd3422d1abb22cff1b8c9d
Simco József <simcojozsef@gmail.com>
2026-05-12 10:54:37 +0200

    Fix syntax highlighter import

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import Image from "next/image";
 
 
